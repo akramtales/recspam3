eb3cec95c33c2f1ad35253d4604ff64ab2176409
akramtales <45820646+akramtales@users.noreply.github.com>
2019-06-15 19:14:59 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("522076287611830282")
+setInterval(function() {
+channel.send(`akramtmdz`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
